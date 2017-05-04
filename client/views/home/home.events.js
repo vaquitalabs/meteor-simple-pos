006136090ff6f2bd84bae33d7cb8e5d59065cf63
davidbalor <david.balderrama.dev@gmail.com>
2017-05-03 20:23:54 -0600

    Show the order in the view

--- client/views/home/home.events.js
+++ client/views/home/home.events.js
@@ -1,17 +1,34 @@
+var order = [];
+var filter = {}
+
 Template.home.events({
 	'click .add-product':(ev)=>{
 		var id = $(ev.target).parent().find('.product-id').val();
 		var product = Products.findOne({_id:id});
-		if ($('#pre-order').find('.' + product._id).length === 0) {
-			$('#pre-order').append("<div class='" + product._id + "'></div>");
-			$('.' + product._id).append("<div class='pre-order-product'>" + product.name + "</div>");
-			$('.' + product._id).append("<div class='pre-order-product'>" + product.category + "</div>");
-			$('.' + product._id).append("<div class='pre-order-product'>" + product.price/100 + "</div>");
-			$('.' + product._id).append("<div val='1' class='qty'>" + 1 + "</div>"); 
-		}else if($('#pre-order').find('.' + product._id).length !== 0){
-			var qty = $('#pre-order .'+ product._id +' .qty');
-			console.log('#pre-order .'+ product._id +' .qty');
-			$('#pre-order .'+ product._id +' .qty').val(qty);
+		var orderProduct = {
+			productName:product.name,
+			productPrice:product.price/100,
+			productId:product._id,
+			productCategory:product.category,
+			productQuantity:1
+		};
+		var exist = false;
+		if (order.length > 0) {
+			for (var i = 0; i <= order.length - 1; i++) {
+				if (orderProduct.productId === order[i].productId) {
+					exist = true;
+					order[i].productQuantity ++;
+					break;
+				}
+			}
+			
 		}
+
+		if (!exist) {
+			order.push(orderProduct);
+		}
+
+		Session.set('order',order);
+
 	}
 });
--- client/views/home/home.helpers.js
+++ client/views/home/home.helpers.js
@@ -1,5 +1,28 @@
-/*Template.home.helpers({
-	realPrice:(price)=>{
-		return price / 100;
+Template.home.helpers({
+	orderProducts:()=>{
+		var products = Session.get('order');
+		var prodOrder = [];
+		if (products.length > 0) {
+			for (var i = 0; i <= products.length - 1; i++) {
+				prodOrder.push({
+					name:products[i].productName,
+					quantity:products[i].productQuantity,
+					unit:products[i].productPrice,
+					subtotal:products[i].productPrice * products[i].productQuantity,
+					category:products[i].productCategory
+				});	
+			}
+		}
+		var total = 0;
+		if (prodOrder.length > 0) {
+			for (var i = 0; i <= prodOrder.length - 1; i++) {
+				total = total + prodOrder[i].subtotal;
+			}
+		}
+		Session.set('total',total);
+		return prodOrder;
+	},
+	totalOrder:()=>{
+		return Session.get('total');
 	}
-});*/
+});
