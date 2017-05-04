84dddde1480cc5cc7913c21e173105b1fdd34678
davidbalor <david.balderrama.dev@gmail.com>
2017-05-04 07:55:09 -0600

    Add function to the button rm-order

--- client/views/home/home.events.js
+++ client/views/home/home.events.js
@@ -30,5 +30,23 @@ Template.home.events({
 
 		Session.set('order',order);
 
+	},
+	'click .rm-product':(ev)=>{
+		var id = $(ev.target).parent().find('.product-id').val();
+		var exist = false;
+		if (order.length > 0) {
+			for (var i = 0; i <= order.length - 1; i++) {
+				if (id === order[i].productId) {
+					exist = true;
+					order[i].productQuantity --;
+					if (order[i].productQuantity === 0) {
+						order.splice(i,1);
+					}
+					break;
+				}
+			}
+			
+		}
+		Session.set('order',order);
 	}
 });
